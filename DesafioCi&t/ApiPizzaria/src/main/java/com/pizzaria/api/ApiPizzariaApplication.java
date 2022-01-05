41a2da0a6ee52f9a9576a8330c20a50e26048437
obrunorm <87882015+obrunorm@users.noreply.github.com>
2022-01-04 23:41:33 -0300

    Add configurações do Swagger e sua documentação

--- DesafioCi&t/ApiPizzaria/src/main/java/com/pizzaria/api/ApiPizzariaApplication.java
+++ DesafioCi&t/ApiPizzaria/src/main/java/com/pizzaria/api/ApiPizzariaApplication.java
@@ -2,12 +2,19 @@ package com.pizzaria.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ApiPizzariaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiPizzariaApplication.class, args);
-	}
-
+@GetMapping
+public ModelAndView swaggerUi() {
+return new ModelAndView("redirect:/swagger-ui/");
+}
+public static void main(String[] args) {
+SpringApplication.run(ApiPizzariaApplication.class, args);
+}
 }
